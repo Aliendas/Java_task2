f8371759b3f3dd25b7de5dcd7f0a559da8ef29a9
Alena Nik <alyona_nik@mail.ru>
2023-04-12 15:45:46 +0500

    first commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int ammount = 13676;
+        int onemile = 20;
+        int bonus = ammount / onemile;
+
+        System.out.println(bonus);
+    }
+}
